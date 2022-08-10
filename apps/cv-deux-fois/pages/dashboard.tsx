7f2ca7815ebd01624f5ab09db182da70acf34ebb
DeuxFois <deuxfois@pop-os.localdomain>
2022-08-10 16:40:49 +0200

    temp commit vercel

--- apps/cv-deux-fois/pages/dashboard.tsx
+++ apps/cv-deux-fois/pages/dashboard.tsx
@@ -51,13 +51,13 @@ export default function Dashboard ({techs, icons, projects}) {
 
 
 export async function getServerSideProps() {
-    // const port = process.env.PORT || 4200;
-    // const techs = await fetch(`http://localhost:${port}/api/techs`).then(res => res.json());
-    // const icons = await fetch(`http://localhost:${port}/api/icons`).then(res => res.json());
-    // const projects = await fetch(`http://localhost:${port}/api/projects`).then((res) => res.json());
-    const techs = await fetch(`https://${process.env.VERCEL_URL}/api/techs`).then(res => res.json());
-    const icons = await fetch(`https://${process.env.VERCEL_URL}/api/icons`).then(res => res.json());
-    const projects = await fetch(`https://${process.env.VERCEL_URL}/api/projects`).then((res) => res.json());
+    const port = process.env.PORT || 4200;
+    const techs = await fetch(`http://localhost:${port}/api/techs`).then(res => res.json());
+    const icons = await fetch(`http://localhost:${port}/api/icons`).then(res => res.json());
+    const projects = await fetch(`http://localhost:${port}/api/projects`).then((res) => res.json());
+    // const techs = await fetch(`https://${process.env.VERCEL_URL}/api/techs`).then(res => res.json());
+    // const icons = await fetch(`https://${process.env.VERCEL_URL}/api/icons`).then(res => res.json());
+    // const projects = await fetch(`https://${process.env.VERCEL_URL}/api/projects`).then((res) => res.json());
     return {
         props: {
             techs: techs.data,
