c952aff0f6e30117a1afa5e65a11f97805462693
DeuxFois <deuxfois@pop-os.localdomain>
2022-08-20 13:50:38 +0200

    test perf2

--- apps/cv-deux-fois/pages/index/techs.tsx
+++ apps/cv-deux-fois/pages/index/techs.tsx
@@ -1,5 +1,4 @@
 import Devicon from '../../components/devicon';
-import { Fade } from "react-awesome-reveal";
 
 
 export default function Techs(props: {
@@ -49,7 +48,6 @@ export default function Techs(props: {
       <div className="flex-grid flex-center">
         {data.map(({ tech, iconss }, index) => (
           <div key={tech.value} className="grid-column">
-            <Fade triggerOnce delay={index*100} direction='up'>
               <h2 className="title-overview"> {tech.value} </h2>
               <p className="text-desc" style={{marginBottom:'20px'}}> {tech.text} </p>
             
@@ -62,7 +60,6 @@ export default function Techs(props: {
                 />
               </div>
             ))}
-            </Fade>
           </div>
         ))}
         </div>
