7d9fdf07d898a65e21a17fdfa36d25edaaab8004
DeuxFois <deuxfois@pop-os.localdomain>
2022-08-24 12:38:53 +0200

    fix css blog

--- apps/cv-deux-fois/pages/blog.tsx
+++ apps/cv-deux-fois/pages/blog.tsx
@@ -3,7 +3,7 @@ export default function Blog() {
 
     return (
         <iframe  id='blog' src="https://deuxfois.github.io/quartz/"
-        sandbox="allow-same-origin"
+
 
         // style={{"width": "100vw", "height": "100vh", "position": "absolute", "top": "0", "left": "0", "border": "none"}}
         // style={{"width": "100%", "height": "100vh", "border": "none", marginTop:'-400px',zIndex:'1',overflowY:'hidden'}}
